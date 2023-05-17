6910be321e274b7116cc4e381b62dac82604c95c
Sukhman Sandhu <sandhu.1@iitj.ac.in>
2023-05-17 21:11:44 +0530

    different todolists create and learn layouts

--- PROJECTS/toDoList-v1/app.js
+++ PROJECTS/toDoList-v1/app.js
@@ -3,20 +3,58 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
 
 app.set('view engine','ejs');
 
+
 const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+let items = ['Buy Food',"Cook Food","Eat Food"];
+let workItems = [];
 
 app.get('/', (req, res) => {
 
     const date = new Date();
-    const dayNo = date.getDay();
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+    };
 
-    const day = daysOfWeek[dayNo];
+    const day = date.toLocaleDateString('en-US',options);
 
-    res.render("list",{dayOfWeek: day});
+    res.render("list",{listType: day,newItem: items});
 
+})
+
+app.post('/',function(req,res){
+    
+    let item = req.body.newItem;
+
+    if(req.body.list === 'Work'){
+        workItems.push(item);
+        res.redirect('/work');
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+    
+})
+
+app.get('/work',function(req,res){
+    res.render('list',{listType: "Work",newItem: workItems});
+})
+
+app.post('/work',function(req,res){
+    
+    res.redirect('/work');
+})
+
+app.get('/about',function(req,res){
+    res.render('about');
 })
 
 app.listen(3000, (req,res)=>{
