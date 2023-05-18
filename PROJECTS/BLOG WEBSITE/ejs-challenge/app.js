abd4ff93fca0078abaf04f2f2043dfa1fb46ceb0
Sukhman Sandhu <sandhu.1@iitj.ac.in>
2023-05-18 16:47:58 +0530

    Made compose page

--- PROJECTS/BLOG WEBSITE/ejs-challenge/app.js
+++ PROJECTS/BLOG WEBSITE/ejs-challenge/app.js
@@ -18,7 +18,19 @@ app.use(express.static("public"));
 app.get('/',function(req,res){
   res.render('home',{homeContent:homeStartingContent});
 })
-  
+
+app.get('/contact',function(req,res){
+  res.render('contact',{contactContent:contactContent})
+})
+
+app.get('/about',function(req,res){
+  res.render('about',{aboutContent:aboutContent})
+})
+
+app.get('/compose',function(req,res){
+  res.render('compose')
+})
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
