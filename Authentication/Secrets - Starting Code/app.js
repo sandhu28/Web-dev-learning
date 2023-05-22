d85df388c44d8e7dc54405771f85d78fc4b3c695
Sukhman Sandhu <sandhu.1@iitj.ac.in>
2023-05-22 17:37:48 +0530

    cookies, sessions using express-session passport

--- Authentication/Secrets - Starting Code/app.js
+++ Authentication/Secrets - Starting Code/app.js
@@ -6,7 +6,11 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const encrypt = require('mongoose-encryption');
 const _ = require("lodash");
-const md5 = require('md5');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const saltRounds = 10;
 
 const app = express();
 
@@ -16,6 +20,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
@@ -23,11 +36,18 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 
 //uses AES encrytion scheme
 // userSchema.plugin(encrypt, { secret: process.env.KEY, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
     res.render('home');
@@ -41,37 +61,96 @@ app.get("/register", function (req, res) {
     res.render('register');
 });
 
+app.get('/secrets', function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render('secrets');
+    }
+    else {
+        res.redirect('/login');
+    }
+});
+
+app.get('/logout', function (req, res) {
+    req.logout(function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+
+            res.redirect('/');
+        }
+    });
+
+});
+
 app.post('/register', function (req, res) {
 
-    const user = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-
-    user.save()
-        .then(function () {
-            res.render('secrets');
-        }
-        )
-        .catch(function (err) {
+    User.register({ username: req.body.username }, req.body.password)
+        .then(function (user) {
+            console.log(user);
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            })
+        })
+        .catch((err) => {
             console.log(err);
         });
+
+    // bcrypt.hash(req.body.password, 10, function (err, hash) {
+    //     const user = new User({
+    //         email: req.body.username,
+    //         password: hash
+    //     })
+
+    //     user.save()
+    //         .then(function () {
+    //             res.render('secrets');
+    //         }
+    //         )
+    //         .catch(function (err) {
+    //             console.log(err);
+    //         });
+
+    // })
+
+
 });
 
 app.post('/login', function (req, res) {
 
-    User.findOne({ email: req.body.username })
-        .then(function (user) {
-            if (user.password === md5(req.body.password)) {
-                res.render('secrets');
-            }
-            else {
-                console.log("Invalid login credentials");
-            }
-        })
-        .catch(function (err) {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    req.login(user, function (err) {
+        if (err) {
             console.log(err);
-        });
+        }
+        else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/secrets");
+            })
+        }
+    })
+
+    // User.findOne({ email: req.body.username })
+    //     .then(function (user) {
+    //         // compare using bcrypt
+    //         bcrypt.compare(req.body.password, user.password, function (err, result) {
+    //             if (result === true) {
+    //                 res.render('secrets');
+    //             }
+    //             else {
+    //                 console.log("Invalid login credentials");
+    //             }
+    //         })
+    //     })
+    //     .catch(function (err) {
+    //         console.log(err);
+    //     });
+
+
 })
 
 
