cb42442a01fc61063cfb94a7074883ba4d1d0fdb
Sukhman Sandhu <sandhu.1@iitj.ac.in>
2023-05-25 00:12:39 +0530

    getting along with React Components

---
+++ my-react-app/src/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List() {
+    return (
+        <ul>
+            <li>Bat</li>
+            <li>Gloves</li>
+            <li>Ball</li>
+            <li>Pad</li>
+            <li>Guard</li>
+        </ul>
+    );
+}
+
+export default List;
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,57 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const customStyle = {
-    color: "red",
-}
-
-function cL(toLog) {
-    console.log(toLog);
-}
-
-const currDate = new Date(2023, 4, 30, 119);
-cL(currDate);
-var hour = currDate.getHours();
-cL(hour);
-
-let greeting = "";
-
-if (hour >= 0 && hour < 12) {
-    greeting = "Good Morning!";
-    customStyle.color = "red";
-}
-else if (hour >= 12 && hour < 18) {
-    greeting = "Good Afternoon!";
-    customStyle.color = "green";
-}
-else {
-    greeting = "Good Night!";
-    customStyle.color = "blue";
-}
+import App from "./App";
 
 ReactDOM.render(
-
-    <div>
-        <h1 className="heading" style={customStyle} >{greeting}</h1>
-    </div>
-    ,
-    document.getElementById('root')
+    <App />,
+    document.getElementById("root")
 );
-
-
-// function MyComponent() {
-//     const name = 'Sukhman';
-//     const showGreeting = true;
-
-//     return (
-//         <div>
-//             {showGreeting && <h1>Hello, {name}!</h1>}
-//             {name.length > 5 ? <p>Your name is long.</p> : <p>Your name is short.</p>}
-//         </div>
-//     );
-// }
-
-// ReactDOM.render(
-//     <MyComponent />,
-//     document.getElementById('root')
-// );
