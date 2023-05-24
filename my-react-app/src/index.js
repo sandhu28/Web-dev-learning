b1c9f7a67ab9ced1f6c31ec882125f190df5573c
Sukhman Sandhu <sandhu.1@iitj.ac.in>
2023-05-25 00:37:04 +0530

    import export ES6

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/App";
+import add, { multiply, subtract, divide } from "./calculator"
+
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById("root")
+// );
 
 ReactDOM.render(
-    <App />,
+    <ul>
+        <li>{add(1, 2)}</li>
+        <li>{multiply(2, 3)}</li>
+        <li>{subtract(7, 2)}</li>
+        <li>{divide(5, 2)}</li>
+    </ul>,
     document.getElementById("root")
 );
