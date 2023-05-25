10204d1c916a3b987452091eab1bf924361e806f
Sukhman Sandhu <sandhu.1@iitj.ac.in>
2023-05-25 23:37:58 +0530

    map intro

--- my-react-app/src/components/App.jsx
+++ my-react-app/src/components/App.jsx
@@ -3,13 +3,29 @@ import Card from "./Card";
 import contacts from "./contact.js";
 import Avatar from "./Avatar";
 
+function createCard(contact) {
+    return (
+        <Card
+            key={contact.id}
+            name={contact.name}
+            img={contact.imgURL}
+            tel={contact.phone}
+            email={contact.email}
+        />
+    );
+}
+
 function App() {
     return (
         <div>
             <h1 className="heading">My Contacts</h1>
             <Avatar img="https://media.licdn.com/dms/image/C4D03AQEbJfB8yK8w6Q/profile-displayphoto-shrink_800_800/0/1636460051304?e=2147483647&v=beta&t=hpsO-uqINPJwosC23pqZvjd4Fcc3ZBxvg6g2PC5hnGg" />
 
-            <Card
+            {
+                contacts.map(createCard)
+            }
+
+            {/* <Card
                 name={contacts[0].name}
                 img={contacts[0].imgURL}
                 tel={contacts[0].phone}
@@ -26,7 +42,7 @@ function App() {
                 img={contacts[2].imgURL}
                 tel={contacts[2].phone}
                 email={contacts[2].email}
-            />
+            /> */}
         </div>
     );
 }
