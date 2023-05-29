30f5593d97207393f06ea6c4d9ac483302fbc64c
Sukhman Sandhu <sandhu.1@iitj.ac.in>
2023-05-29 23:27:42 +0530

    keeper App part 3

--- my-react-app/src/components/App.jsx
+++ my-react-app/src/components/App.jsx
@@ -1,48 +1,42 @@
-import React from "react";
-import Card from "./Card";
-import contacts from "./contact.js";
-import Avatar from "./Avatar";
-
-function createCard(contact) {
-    return (
-        <Card
-            key={contact.id}
-            name={contact.name}
-            img={contact.imgURL}
-            tel={contact.phone}
-            email={contact.email}
-        />
-    );
-}
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote) {
+        setNotes(prevNotes => {
+            return [...prevNotes, newNote];
+        });
+    }
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    }
+
     return (
         <div>
-            <h1 className="heading">My Contacts</h1>
-            <Avatar img="https://media.licdn.com/dms/image/C4D03AQEbJfB8yK8w6Q/profile-displayphoto-shrink_800_800/0/1636460051304?e=2147483647&v=beta&t=hpsO-uqINPJwosC23pqZvjd4Fcc3ZBxvg6g2PC5hnGg" />
-
-            {
-                contacts.map(createCard)
-            }
-
-            {/* <Card
-                name={contacts[0].name}
-                img={contacts[0].imgURL}
-                tel={contacts[0].phone}
-                email={contacts[0].email}
-            />
-            <Card
-                name={contacts[1].name}
-                img={contacts[1].imgURL}
-                tel={contacts[1].phone}
-                email={contacts[1].email}
-            />
-            <Card
-                name={contacts[2].name}
-                img={contacts[2].imgURL}
-                tel={contacts[2].phone}
-                email={contacts[2].email}
-            /> */}
+            <Header />
+            <CreateArea onAdd={addNote} />
+            {notes.map((noteItem, index) => {
+                return (
+                    <Note
+                        key={index}
+                        id={index}
+                        title={noteItem.title}
+                        content={noteItem.content}
+                        onDelete={deleteNote}
+                    />
+                );
+            })}
+            <Footer />
         </div>
     );
 }
